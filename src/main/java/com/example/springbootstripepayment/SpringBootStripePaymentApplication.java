8aeaea8fa4a93ceb6c239ede9b0e674b62ce4223
neerajparkashsharma <npsstudies@gmail.com>
2023-05-13 12:40:31 +0500

    initial commit

---
+++ src/main/java/com/example/springbootstripepayment/SpringBootStripePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootstripepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStripePaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStripePaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootstripepayment/SpringBootStripePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootstripepayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStripePaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
